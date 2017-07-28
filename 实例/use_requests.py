f778b70b494f38662352b5be1f219310c723023b
caiiiac <caiiiac@163.com>
2017-07-28 22:32:05 +0800

    ip查询

--- 实例/use_requests.py
+++ 实例/use_requests.py
@@ -12,15 +12,15 @@ import requests
 
 
 # 亚马逊
-url = "https://www.amazon.cn/gp/product/B0094DVNT6"
-try:
-	kv = {'User-Agent' : 'Mozilla/5.0'}
-	r = requests.get(url, headers=kv)
-	r.raise_for_status()
-	r.encoding = r.apparent_encoding
-	print(r.text[:1000])
-except:
-	print("爬虫失败")
+# url = "https://www.amazon.cn/gp/product/B0094DVNT6"
+# try:
+# 	kv = {'User-Agent' : 'Mozilla/5.0'}
+# 	r = requests.get(url, headers=kv)
+# 	r.raise_for_status()
+# 	r.encoding = r.apparent_encoding
+# 	print(r.text[:1000])
+# except:
+# 	print("爬虫失败")
 
 
 # 百度搜索
@@ -37,20 +37,33 @@ except:
 
 # 图片爬取
 import os
-url = "http://www.pp3.cn/uploads/201610/2016103006.jpg"
-root = "/Users/caiiiac/Desktop/"
-path = root + url.split('/')[-1]
+# url = "http://www.pp3.cn/uploads/201610/2016103006.jpg"
+# root = "/Users/caiiiac/Desktop/"
+# path = root + url.split('/')[-1]
 
+# try:
+# 	if not os.path.exists(root):
+# 		os.mkdir(root)
+# 	if not os.path.exists(path):
+# 		r = requests.get(url)
+# 		with open(path, 'wb') as f:
+# 			f.write(r.content)
+# 			f.close()
+# 			print('文件保存成功')
+# 	else :
+# 		print('文件已经存在')
+# except:
+# 	print('爬虫失败')
+
+
+
+#IP地址查询全代码
+import requests
+url = "http://m.ip138.com/ip.asp?ip="
 try:
-	if not os.path.exists(root):
-		os.mkdir(root)
-	if not os.path.exists(path):
-		r = requests.get(url)
-		with open(path, 'wb') as f:
-			f.write(r.content)
-			f.close()
-			print('文件保存成功')
-	else :
-		print('文件已经存在')
+	r = requests.get(url+'202.204.80.112')
+	r.raise_for_status()
+	r.encoding = r.apparent_encoding
+	print(r.text)
 except:
-	print('爬虫失败')
+	print("爬取失败")
